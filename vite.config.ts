73538099ea62e79d9724cabe3005c195f65c13f1
YHStar <980506422@qq.com>
2025-06-26 01:44:05 +0800

    Adj Path

--- vite.config.ts
+++ vite.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
       extensions: ['vue', 'md'],
     }),
   ],
-  base: './',
+  base: '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
