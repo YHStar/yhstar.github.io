57ad575e39ac6adef8d96a255fba9d5ea481d6f2
YHStar <980506422@qq.com>
2025-06-26 05:58:28 +0800

    add styles

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,98 @@
 <template>
-  <header class="flex">
-    <nav class="navbar backpic">
-      <div class="navbar-brand ">
-        <h1 class="navbar-item is-light is-size-3">Away's Blog</h1>
+  <header>
+    <div class="header-content">
+      <!-- Left side: Away's Blog -->
+      <h1 class="left">Away's Blog</h1>
+      <div class="right">
+        <RouterLink to="/" class="button is-transparent is-light is-outlined">Home</RouterLink>
+        <RouterLink to="/about" class="button is-transparent is-light is-outlined">About</RouterLink>
       </div>
-      <!-- 将 Home 和 About 按钮放到同一行 -->
-      <div class="navbar-end">
-        <div class="navbar-item buttons">
-          <RouterLink to="/" class="button is-transparent is-light is-outlined">Home</RouterLink>
-          <RouterLink to="/about" class="button is-transparent is-light is-outlined">About</RouterLink>
-        </div>
-      </div>
-    </nav>
+    </div>
   </header>
 
   <main class="section">
     <dev class="container mx-auto py-8 px-4">
-      <!-- 注意 router-view 是动态加载当前路由组件的插槽 -->
       <router-view v-slot="{ Component }">
-        <!-- 使用动态组件确保每次切换都会重新渲染 -->
         <component :is="Component" />
       </router-view>
     </dev>
   </main>
 
-  <footer class="flex is-center text-sm text-gray-500 py-6 border-t">
-    2025 Away的博客
+  <footer class="is-center">
+    Copyright &copy; 2025 Away's Blog
   </footer>
 </template>
 
 <script setup>
-// 这里可以引入全局状态或初始化逻辑
+
 </script>
 
 <style>
-.backpic {
-  height: 300px;
+/* 确保导航项水平排列 */
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+/* 导航按钮容器 */
+.navbar-end {
+  display: flex;
+  justify-content: flex-end;
+  /* 将导航按钮向右对齐 */
+}
+
+p {
+  text-indent: 2em;
+  /* 将段落首行缩进 2 个字符宽度 */
+}
+
+header {
+  position: fixed;
+  top: 0;
+  background-color: rgba(0, 0, 0, 0.3);
+  width: 100%;
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.header-content {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+}
+
+.left {
+  color: #fff;
+  font-size: 24px;
+}
+
+.right {
+  display: flex;
+  gap: 10px;
+}
+html,
+body {
+  height: 100%;
   margin: 0;
-  background-image: url('./background.jpg');
+  padding: 0;
+  background-image: url('/background.jpg');
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
+}
+footer {
+  position: fixed;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.3);
+  width: 100%;
+  height: 40px;
+  padding: 5px 5px;
+  margin-top: 10px;
+  color: gray;
+  text-align: center;
 }
 </style>
--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,7 +1,10 @@
 <template>
-    <p>这是测试内容</p>
+  
+  <p>很早就想做属于自己的博客了，但是小时候没有自己的服务器也没有自己的域名。在大学，我了解到可以通过Github-Pages来免费托管网站，又恰逢JS大作业，于是完成自己从小的一份愿望，编写了这份个人博客。网页是用Vue+Vite从头编写的，主要学习资料来自于ChatGPT，欢迎大家查阅源码，也欢迎大家指点。</p>
 </template>
 
+<style>
+</style>
 <script>
 export default {
   name: 'About'
