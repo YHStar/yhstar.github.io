e32e67343fa7031f02ab28428e4cbe22b9a93129
YHStar <980506422@qq.com>
2025-06-26 05:03:35 +0800

    add background

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,32 @@
 <template>
-  <div>
-    <header>
-      <ul>
-        <h1>Away的博客</h1>
-        <div>
-          <li class="list-none">
-            <RouterLink to="/">Home</RouterLink>
-          </li>
-          <li class="list-none">
-            <RouterLink to="/about">About</RouterLink>
-          </li>
+  <header class="flex">
+    <nav class="navbar backpic">
+      <div class="navbar-brand ">
+        <h1 class="navbar-item is-light is-size-3">Away's Blog</h1>
+      </div>
+      <!-- 将 Home 和 About 按钮放到同一行 -->
+      <div class="navbar-end">
+        <div class="navbar-item buttons">
+          <RouterLink to="/" class="button is-transparent is-light is-outlined">Home</RouterLink>
+          <RouterLink to="/about" class="button is-transparent is-light is-outlined">About</RouterLink>
         </div>
-      </ul>
-    </header>
+      </div>
+    </nav>
+  </header>
 
-    <main class="container mx-auto py-8 px-4">
+  <main class="section">
+    <dev class="container mx-auto py-8 px-4">
       <!-- 注意 router-view 是动态加载当前路由组件的插槽 -->
       <router-view v-slot="{ Component }">
         <!-- 使用动态组件确保每次切换都会重新渲染 -->
         <component :is="Component" />
       </router-view>
-    </main>
+    </dev>
+  </main>
 
-    <footer class="text-center text-sm text-gray-500 py-6 border-t">
-      2025 Away的博客
-    </footer>
-  </div>
+  <footer class="flex is-center text-sm text-gray-500 py-6 border-t">
+    2025 Away的博客
+  </footer>
 </template>
 
 <script setup>
@@ -33,5 +34,12 @@
 </script>
 
 <style>
-/* 可选的全局样式 */
+.backpic {
+  height: 300px;
+  margin: 0;
+  background-image: url('./background.jpg');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+}
 </style>
